dabd1256a45ca420ca8a6e239b892a5271f1ce83
Lutfiana443 <lutfianadwi853@gmail.com>
2025-02-04 11:25:34 +0700

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama = "Lutfiana Dwi Lestari";
+    int nis = 24322;
+    string nisn = "3093773607";
+    string ttl = "Semarang, 11 Maret 2009";
+    string tl= "SEMARANG";
+    string agama = "Islam";
+    string status = "Anak";
+    int anak = 2;
+    string alamat = "Jl.Bukit Beringin Elok II B275";
+    string smp = "SMP Negeri 28 Semarang";
+    string kota = "Semarang";
+    string no = "085803617829";
+    string ayah = "Sumaryanto";
+    string ibu = "ada";
+    string payah = "Wirausaha";
+    string pibu = "Buruh";
+
+    cout << "Nama : " << nama << endl;
+    cout << "NIS : " << nis << endl;
+    cout << "NISN : " << nisn << endl;
+    cout << "Tempat Tanggal Lahir : " << ttl << endl;
+    cout << "Tempat Lahir : " << tl << endl;
+    cout << "Agama : " << agama << endl;
+    cout << "Status dalam keluarga : " << status << endl;
+    cout << "Anak Ke : " << anak << endl;
+    cout << "Alamat : " << alamat << endl;
+    cout << "Asal SMP : " << smp << endl;
+    cout << "No Telpon : " << no << endl;
+    cout << "Nama Ayah : " << ayah << endl;
+    cout << "Nama Ibu : " << ibu << endl;
+    cout << "Pekerjaan Ayah : " << payah << endl;
+    cout << "Pekerjaan Ibu : " << pibu << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
